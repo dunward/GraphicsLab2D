afb1621ef2abc5dd19db61044b56f5fa4e310743
weisswolfi <wolfi@bytenine.io>
2020-02-03 12:00:27 +0900

    feat: Add Ink Splash VFX

---
+++ Assets/VFX/Ink Splash/InkSplash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkSplash : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+
+    [SerializeField]
+    private AnimationCurve curve;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Animate());
+        }
+    }
+
+    private IEnumerator Animate()
+    {
+        float t = 0;
+        while(curve.Evaluate(t) < 1f)
+        {
+            t += Time.deltaTime;
+            material.SetFloat("_Delta", curve.Evaluate(t));
+            yield return null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<p align="center"><img src ="https://img.shields.io/badge/unity-2019.3.0f6-brightgreen?style=flat-square&logo=unity&logoColor=white"/> <img src="https://img.shields.io/badge/pipeline-universal-blue?style=flat-square"></p>
+<p align="center"><img src ="https://img.shields.io/badge/unity-2019.3.0f6-brightgreen?style=flat-square&logo=unity&logoColor=white"/> <img src="https://img.shields.io/badge/pipeline-universal-blue?style=flat-square"><br><br><image src="Docs/Ink%20Splash/ink%20splash.gif"></p>
 
-# GraphicsLab
+# GraphicsLab2D
 **require Universal Render Pipeline**
 
 🎨 Awesome visual effects on Unity.
